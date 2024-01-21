f81b8fcc61be402e51dd9e197bdcd091b0a82fd6
sophiarchen <sophiachen@ucdavis.edu>
2024-01-20 17:39:01 -0800

    update

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -1,2 +1,2 @@
 #Authors: Sophia Chen, Devin Fan
-gunzip -c dictionary.gz | grep "r" | grep -E "[zoniacr]{4,}" | grep -v "[qwetyupsdfghjklxvbm]" | wc
+gunzip -c dictionary.gz | grep "r" | grep -E "[zoniacr]{4,}" | grep -v "[qwetyupsdfghjklxvbm]"
