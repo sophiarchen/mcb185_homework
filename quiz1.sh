5fd4ebffa31323e8c7a3746735f90688c3123fd5
sophiarchen <sophiachen@ucdavis.edu>
2024-01-23 09:23:53 -0800

    update

--- quiz1.sh
+++ quiz1.sh
@@ -1,4 +1,7 @@
-Name: Sophia Chen
-Linux User Name: sophiarchen
+echo "Name: Sophia Chen"
+echo "Linux User Name: $USER"
 
-sh task3.sh
+gunzip -c dictionary.gz | grep "a" | grep -E "[muocfta]{4,}" | grep -v "[^muocfta]" | wc
+gunzip -c dictionary.gz | grep "b" | grep -E "[tairnlb]{4,}" | grep -v "[^tairnlb]" | wc
+gunzip -c dictionary.gz | grep "c" | grep -E "[maodinc]{4,}" | grep -v "[^maodinc]" | wc
+gunzip -c dictionary.gz | grep "z" | grep -E "[anoigrz]{4,}" | grep -v "[^anoigrz]" | wc
