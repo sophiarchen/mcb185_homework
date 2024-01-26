f8472e90d851eabe9519c296cb57ed0b1b025890
sophiarchen <sophiachen@ucdavis.edu>
2024-01-26 15:52:18 -0800

    new

---
+++ 20demo.py
@@ -0,0 +1,27 @@
+# 20demo.py by Sophia Chen
+
+import math 
+
+print('hello, again') #greeting
+
+print(1.5e-2)
+
+print(9%2)
+print(9//2)
+print (9/2)
+
+print(3**3)
+
+print(1 + 1)
+print ('1' + '1')
+
+print(math.ceil(2.71))
+print(math.floor(2.71))
+
+print (0.1*1)
+print(0.1*3)
+
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+print(c)
