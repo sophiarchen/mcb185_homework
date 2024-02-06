7df76ae4fc9c8731883fe21ca088537ec3b8e2cd
sophiarchen <sophiachen@ucdavis.edu>
2024-02-06 09:15:12 -0800

    update

--- 38quiz.py
+++ 38quiz.py
@@ -1,5 +1,6 @@
 #Co-Authors: Sophia Chen and Devin Fan
 
+"""
 pi = 0
 const = 1 
 x = 1
@@ -10,3 +11,26 @@ for i in range(0, 100000):
 	const = const * -1
 pi = pi * 4
 print(pi)
+"""
+
+def pi_comp(n):
+	pi_gl = 0
+	const_gl = 1
+	x_gl = 1
+	pi_ni = 3
+	const_ni = 4
+	x_ni = 2
+	y_ni = 3
+	z_ni = 4
+	for i in range(0, n):
+		pi_gl = pi_gl + (const_gl / x_gl)
+		x_gl = x_gl + 2
+		const_gl = const_gl * -1
+		pi_ni = pi_ni + (const_ni / (x_ni * y_ni * z_ni))
+		x_ni = x_ni + 2
+		y_ni = y_ni + 2
+		z_ni = z_ni + 2
+		const_ni = const_ni * -1
+		print(pi_ni, 4*pi_gl)
+		
+pi_comp(10)
