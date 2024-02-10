4be0bf94baee7bd0c1d2b24a606ab253723837ee
sophiarchen <sophiachen@ucdavis.edu>
2024-02-10 00:34:50 -0800

    update

--- 42chicago.py
+++ 42chicago.py
@@ -1,14 +1,13 @@
 #42chicago.py by Sophia Chen
 
 import random
-import sys
 
 games= 1000000
 log = games // 100 
 zero_games = 0
 total = 0
 for i in range(games):
-	if i % log == 0: print(f'{100 * i/games:.0f}', file=sys.stderr)
+	if i % log == 0: print(f'{100 * i/games:.0f}')
 	score = 0
 	for target in range(2, 13):
 		d1 = random.randint(1, 6)
