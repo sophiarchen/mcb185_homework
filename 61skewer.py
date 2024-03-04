c04af6c7b3e8281cf97e044a68dbf9048f5f0087
sophiarchen <sophiachen@ucdavis.edu>
2024-03-03 22:41:25 -0800

    unit 6 homework

---
+++ 61skewer.py
@@ -0,0 +1,9 @@
+#61skewer.py by Sophia Chen
+
+import dogma
+
+seq = 'ACGTACGTGGGGGACGTACGTCCCCC'
+w = 10
+for i in range(len(seq) -w + 1):
+	s = seq[i:i+w]
+	print(f'{i}\t{dogma.gc_comp(s):.3f}\t{dogma.gc_skew(s):.3f}')
