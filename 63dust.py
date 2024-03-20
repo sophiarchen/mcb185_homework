f3d01eb20b53588e4b6bc23099076ec42aa9a76b
sophiarchen <sophiachen@ucdavis.edu>
2024-03-20 15:21:11 -0700

    update

--- 63dust.py
+++ 63dust.py
@@ -12,20 +12,19 @@ ntseq = []
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	for nt in seq: 
 		if nt in 'ATCG': ntseq.append(nt)
-ntseq = ''.join(ntseq)
+	ntseq = ''.join(ntseq)
 	
-modseq = []
-for i in range(0, len(ntseq), window_size):
-	chunk = ntseq[i:i+window_size]
-	a = chunk.count('A')
-	t = chunk.count('T')
-	c = chunk.count('C')
-	g = chunk.count('G')
-	h = dogma.entropy(a, t, c, g)
-	if h >= entropy: 
-		for nt in chunk: modseq.append(nt)
-	else: 
-		for nt in chunk: modseq.append('N')
-modseq = ''.join(modseq)
-print(f'>{defline}\t{modseq}')
-	
+	modseq = []
+	for i in range(0, len(ntseq), window_size):
+		chunk = ntseq[i:i+window_size]
+		a = chunk.count('A')
+		t = chunk.count('T')
+		c = chunk.count('C')
+		g = chunk.count('G')
+		h = dogma.entropy(a, t, c, g)
+		if h >= entropy: 
+			for nt in chunk: modseq.append(nt)
+		else: 
+			for nt in chunk: modseq.append('N')
+	modseq = ''.join(modseq)
+	print(f'>{defline}\t{modseq}')
